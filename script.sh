144d982ceb58d6603071a9997ccec32dab792bbe
Juan Carlos Villarroel Alvarez <juancvilla@gmail.com>
2024-10-08 18:06:58 -0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,48 @@
+sudo swapoff -a
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+sudo sed -i '$ a 192.168.1.130 master-node-k8.virtualbox.org' /etc/hosts
+sudo sed -i '$ a 192.168.1.140 worker-node-1-k8.virtualbox.org' /etc/hosts
+sudo dnf install -y iproute-tc
+sudo firewall-cmd --permanent --add-port=6443/tcp
+sudo firewall-cmd --permanent --add-port=2379-2380/tcp
+sudo firewall-cmd --permanent --add-port=10250/tcp
+sudo firewall-cmd --permanent --add-port=10251/tcp
+sudo firewall-cmd --permanent --add-port=10252/tcp
+sudo firewall-cmd --permanent --add-port=30000-32767/tcp                                                 
+sudo firewall-cmd --reload
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+export CRIO_VERSION=v1.30
+cat <<EOF | sudo tee /etc/yum.repos.d/cri-o.repo
+[cri-o]
+name=CRI-O
+baseurl=https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/rpm/repodata/repomd.xml.key
+EOF
+sudo dnf install -y cri-o
+sudo systemctl enable crio
+sudo systemctl start crio
+KUBERNETES_VERSION=v1.30
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/repodata/repomd.xml.key
+EOF
+sudo dnf install kubelet kubeadm kubectl -y
+sudo systemctl enable kubelet
+sudo systemctl start kubelet
